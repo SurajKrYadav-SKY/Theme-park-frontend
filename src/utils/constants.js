5f4f8b2e08614b55a05cbbf15a35badefc754773
Suraj Kumar Yadav <suraj.mails99@gmail.com>
2025-03-05 11:03:38 +0530

    small bug fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const HOST = import.meta.env.VITE_SERVER_URL;
-export const AUTH_ROUTE = import.meta.env.AUTH_ROUTE;
+export const AUTH_ROUTE = import.meta.env.VITE_AUTH_ROUTE;
 
 export const SIGNUP_ROUTE = `${AUTH_ROUTE}/signup`;
 export const LOGIN_ROUTE = `${AUTH_ROUTE}/login`;
